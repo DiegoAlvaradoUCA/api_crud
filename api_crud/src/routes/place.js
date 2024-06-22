756da5a18a659304370769c8dc640ad5699928d3
DiegoAlvaradoUCA <00127522@uca.edu.sv>
2024-06-22 09:19:58 -0600

    API_CRUD

---
+++ api_crud/src/routes/place.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const router = express.Router();
+const Place = require("../models/place");
+
+// Create a place
+router.post("/places", (req, res) => {
+  const { destinationName, location, package, suggestedPrice, category, guide, mainImage, description } = req.body;
+  const newPlace = new Place({
+    destinationName,
+    location,
+    package,
+    suggestedPrice,
+    category,
+    guide,
+    mainImage,
+    description
+  });
+
+  newPlace
+    .save()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// Get all places
+router.get("/places", (req, res) => {
+  Place.find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// Get a place by ID
+router.get("/places/:id", (req, res) => {
+  const { id } = req.params;
+  Place.findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+// Delete a place
+router.delete("/places/:id", (req, res) => {
+  const { id } = req.params;
+  Place.findByIdAndDelete(id)
+    .then(() => res.json({ message: "Place deleted successfully" }))
+    .catch((error) => res.json({ message: error }));
+});
+
+// Update a place
+router.put("/places/:id", (req, res) => {
+  const { id } = req.params;
+  const { destinationName, location, package, suggestedPrice, category, guide, mainImage, description } = req.body;
+  Place.findByIdAndUpdate(id, {
+    destinationName,
+    location,
+    package,
+    suggestedPrice,
+    category,
+    guide,
+    mainImage,
+    description
+  }, { new: true })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+module.exports = router;
